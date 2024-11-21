0222a2eabc95014b013753fd270f700c7b2537c0
jowy99 <jowycarreras@gmail.com>
2024-11-21 11:05:40 +0100

    Mail

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import sendEmailRoute from './src/api/send-email.js';
 const app = express();
 const PORT = 5000;
 
-app.use(cors({ origin: 'http://localhost:4321' }));
+app.use(cors({ origin: 'https://test.joelarnaud.com' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,5 +13,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/send-email', sendEmailRoute);
 
 app.listen(PORT, () => {
-    console.log(`Servidor de API en http://localhost:${PORT}`);
+    console.log(`Servidor de API en https://test.joelarnaud.com:${PORT}`);
 });
