88b329b6bf60cd8f316a82be9b97bf1dbf3b575c
jowy99 <jowycarreras@gmail.com>
2024-12-30 12:15:28 +0100

    astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,15 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: [
+        'express',
+        'body-parser',
+        'cors',
+        'nodemailer',
+        // Agrega aquí otras dependencias que podrían estar causando el problema
+      ],
+    },
+  },
 });
